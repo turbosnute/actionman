cada4efc0c5d21ad41bdf82233931244abfcb550
Øyvind Andreas Nilsen <on@ntnu.no>
2024-07-03 10:58:54 +0200

    added test script

---
+++ run.Tests.ps1
@@ -0,0 +1,12 @@
+$pester_result = Invoke-Pester skript.Tests.ps1 -Passthru
+
+$test_table = "| Test | Result |`n| --- | --- |`n"
+
+foreach ($test in $pester_result.Tests) {
+    $test_table += "| $($test.Name) | $($test.Result) |`n"
+
+}
+
+$test_table
+
+if ($Error[0].Fullyqualifiederrorid -eq 'PesterAssertionFailed') {exit 1}  
