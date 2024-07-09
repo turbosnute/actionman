92bff8574976c137944a4575f7599fdb96fba32e
Øyvind Andreas Nilsen <on@ntnu.no>
2024-07-09 14:33:59 +0200

    summary test nummer 349243

--- run.Tests.ps1
+++ run.Tests.ps1
@@ -8,6 +8,6 @@ foreach ($test in $pester_result.Tests) {
 }
 
 $test_table
-$env:GITHUB_STEP_SUMMARY = $test_table 
+$test_table  >> $env:GITHUB_STEP_SUMMARY
 
 if ($Error[0].Fullyqualifiederrorid -eq 'PesterAssertionFailed') {exit 1}  
